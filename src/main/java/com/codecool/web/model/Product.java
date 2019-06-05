ef8777b808a75f5ca69bf0105f98920beab20a58
Toth-Gabor <upcgabor@gmail.com>
2019-06-05 12:45:52 +0200

    added Product class

--- src/main/java/com/codecool/web/model/Product.java
+++ src/main/java/com/codecool/web/model/Product.java
@@ -1,4 +1,66 @@
 package com.codecool.web.model;
 
-public class Product {
+import java.util.Objects;
+
+public class Product extends AbstractModel {
+    
+    private final String name;
+    private final String brand;
+    private final String specification;
+    private final String description;
+    private final int price;
+    private final String photoUrl;
+    
+    public Product(int id, String name, String brand, String specification, String description, int price, String photoUrl) {
+        super(id);
+        this.name = name;
+        this.brand = brand;
+        this.specification = specification;
+        this.description = description;
+        this.price = price;
+        this.photoUrl = photoUrl;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getBrand() {
+        return brand;
+    }
+    
+    public String getSpecification() {
+        return specification;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public int getPrice() {
+        return price;
+    }
+    
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Product product = (Product) o;
+        return price == product.price &&
+                Objects.equals(name, product.name) &&
+                Objects.equals(brand, product.brand) &&
+                Objects.equals(specification, product.specification) &&
+                Objects.equals(description, product.description) &&
+                Objects.equals(photoUrl, product.photoUrl);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name, brand, specification, description, price, photoUrl);
+    }
 }
