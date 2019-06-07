eeea29f612eaa5590fd1114c4e2870fa3e0f5533
Toth-Gabor <upcgabor@gmail.com>
2019-06-07 13:01:06 +0200

    added new product_quantity_check function and trigger

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,6 +1,9 @@
 /*
     Database initialization script that runs on every web-application redeployment.
 */
+DROP TRIGGER IF EXISTS product_quantity_check on products;
+DROP FUNCTION IF EXISTS product_quantity_check;
+
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS orders CASCADE;
 DROP TABLE IF EXISTS products CASCADE;
@@ -18,13 +21,6 @@ CREATE TABLE users
     CONSTRAINT pw_not_empty CHECK (password <> '')
 );
 
-CREATE TABLE orders
-(
-    order_id SERIAL  NOT NULL PRIMARY KEY,
-    user_id  INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
-);
-
 CREATE TABLE products
 (
     product_id    SERIAL         NOT NULL PRIMARY KEY,
@@ -33,7 +29,15 @@ CREATE TABLE products
     specification TEXT           NOT NULL,
     description   TEXT           NOT NULL,
     price         DECIMAL(10, 2) NOT NULL,
+    quantity      INTEGER        NOT NULL,
     photo_url     TEXT
+);
+
+CREATE TABLE orders
+(
+    order_id SERIAL  NOT NULL PRIMARY KEY,
+    user_id  INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE line_item
@@ -44,6 +48,7 @@ CREATE TABLE line_item
     FOREIGN KEY (order_id) REFERENCES orders (order_id),
     FOREIGN KEY (product_id) REFERENCES products (product_id)
 );
+
 
 CREATE TABLE attributes_table
 (
@@ -57,6 +62,20 @@ CREATE TABLE attributes_table
     FOREIGN KEY (product_id) REFERENCES products (product_id)
 );
 
+CREATE FUNCTION product_quantity_check() RETURNS trigger
+AS '
+    BEGIN
+        IF NEW.quantity < 0 THEN
+            RAISE EXCEPTION ''This product out of stock!'';
+        END IF;
+        RETURN NEW;
+    END; '
+    LANGUAGE plpgsql;
+
+CREATE TRIGGER product_quantity_check BEFORE INSERT OR UPDATE ON products
+    FOR EACH ROW EXECUTE PROCEDURE product_quantity_check();
+
+
 INSERT INTO users (user_name, email, password, administrator)
 VALUES ('Gábor', 'user1@user1', 'user1', TRUE); -- 1
 
@@ -64,31 +83,31 @@ INSERT INTO users (user_name, email, password)
 VALUES ('Péter', 'user2@user2', 'user2'), -- 2
        ('András', 'user2@user3', 'user3'); -- 3
 
-INSERT INTO products (product_name, brand, specification, description, price, photo_url)
+INSERT INTO products (product_name, brand, specification, description, quantity, price, photo_url)
 VALUES ('EF 50mm F1.8 STM lens', 'Canon',
         'Mount: Canon EF | Elements/groups: 6/5 | Diaphragm blades: 7 | Autofocus: Stepping motor | Stabilizer: None | Minimum focus distance: 0.35m | Maximum magnification: 0.21x | Filter thread: 49mm | Dimensions (WxL): 69x39mm | Weight: 160g',
         'A great low-cost portrait lens for APS-C Canon DSLRs',
-        114, 'Canon_EF_50mm_F1.8_STM_lens.jpg'),
+        10, 114, 'Canon_EF_50mm_F1.8_STM_lens.jpg'),
        ('EF-S 17-55mm f2.8 IS USM', 'Canon',
         'Mount: Canon EF-S | Effective zoom range: 27-88mm | Lens construction: 19 elements in 12 groups | No. of diaphragm blades: 7 blades | Minimum focus distance: 0.35m | Filter size: 77mm | Dimensions: 84x111mm | Weight: 645g',
         'A veteran APS-C format lens, with high-quality build',
-        765, 'photos/Canon_EF-S_17-55mm_f2.8_IS_USM.jpg'),
+        11, 765, 'photos/Canon_EF-S_17-55mm_f2.8_IS_USM.jpg'),
        ('EF 24-70mm f2.8L II USM', 'Canon',
         'Mount: Canon EF | Effective zoom range: 24-70mm | Lens construction: 18 elements in 13 groups | No. of diaphragm blades: 9 blades | Minimum focus distance: 0.38m | Filter size: 82mm | Dimensions: 89x113mm | Weight: 805g',
         'This Mk II edition is the reinvention of a classic',
-        1789, 'photos/Canon_EF_24-70mm_f2.8L_II_USM.jpg'),
+        20, 1789, 'photos/Canon_EF_24-70mm_f2.8L_II_USM.jpg'),
        ('EF 24-105mm f4L IS II USM', 'Canon',
         'Mount: Canon EF | Effective zoom range: 24-105mm | Lens construction: 17 elements in 12 groups | No. of diaphragm blades: 10 blades | Minimum focus distance: 0.45m | Filter size: 77m | Dimensions: 84x118mm | Weight: 795g',
         'Another Canon standard zoom gets upgraded',
-        1299, 'photos/Canon_EF_24-105mm_f4L_IS_II_USM.jpg'),
+        5, 1299, 'photos/Canon_EF_24-105mm_f4L_IS_II_USM.jpg'),
        ('Canon Ef front lens cap', 'Canon', '55mm Front Lens Cap', 'Snap-clips',
-        8, 'photos/Canon_lens_cap.jpg'),
+        10, 8, 'photos/Canon_lens_cap.jpg'),
        ('Canon Ef front lens cap', 'Canon', '58mm Front Lens Cap', 'Snap-clips',
-        9, 'photos/Canon_lens_cap.jpg'),
+        20, 9, 'photos/Canon_lens_cap.jpg'),
        ('Canon Ef front lens cap', 'Canon', '67mm Front Lens Cap', 'Snap-clips',
-        10, 'photos/Canon_lens_cap.jpg'),
+        13, 10, 'photos/Canon_lens_cap.jpg'),
        ('Canon Ef front lens cap', 'Canon', '72mm Front Lens Cap', 'Snap-clips',
-        10, 'photos/Canon_lens_cap.jpg');
+        11, 10, 'photos/Canon_lens_cap.jpg');
 
 
 INSERT INTO attributes_table (att_name, text, num, bool, type, product_id)
@@ -96,7 +115,9 @@ VALUES ('Mount', 'EF', NULL, NULL, 'text', 1),
        ('Mount', 'EF-S', NULL, NULL, 'text', 2),
        ('Mount', 'EF', NULL, NULL, 'text', 3),
        ('Mount', 'EF', NULL, NULL, 'text', 4),
-       ('Diameter', null, 55, null, 'num', 5),
-       ('Diameter', null, 58, null, 'num', 6),
-       ('Diameter', null, 67, null, 'num', 7),
-       ('Diameter', null, 72, null, 'num', 8);
+       ('Diameter', NULL, 55, NULL, 'num', 5),
+       ('Diameter', NULL, 58, NULL, 'num', 6),
+       ('Diameter', NULL, 67, NULL, 'num', 7),
+       ('Diameter', NULL, 72, NULL, 'num', 8);
+
+
