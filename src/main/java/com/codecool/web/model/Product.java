0228fbea4308b3a0d9ff00a8ba197897a6f843d5
Toth-Gabor <upcgabor@gmail.com>
2019-06-07 13:22:58 +0200

    added quantity field

--- src/main/java/com/codecool/web/model/Product.java
+++ src/main/java/com/codecool/web/model/Product.java
@@ -9,15 +9,17 @@ public class Product extends AbstractModel {
     private final String specification;
     private final String description;
     private final int price;
+    private final int quantity;
     private final String photoUrl;
     
-    public Product(int id, String name, String brand, String specification, String description, int price, String photoUrl) {
+    public Product(int id, String name, String brand, String specification, String description, int price, int quantity, String photoUrl) {
         super(id);
         this.name = name;
         this.brand = brand;
         this.specification = specification;
         this.description = description;
         this.price = price;
+        this.quantity = quantity;
         this.photoUrl = photoUrl;
     }
     
@@ -41,6 +43,10 @@ public class Product extends AbstractModel {
         return price;
     }
     
+    public int getQuantity() {
+        return quantity;
+    }
+    
     public String getPhotoUrl() {
         return photoUrl;
     }
@@ -52,6 +58,7 @@ public class Product extends AbstractModel {
         if (!super.equals(o)) return false;
         Product product = (Product) o;
         return price == product.price &&
+                quantity == product.quantity &&
                 Objects.equals(name, product.name) &&
                 Objects.equals(brand, product.brand) &&
                 Objects.equals(specification, product.specification) &&
@@ -61,6 +68,6 @@ public class Product extends AbstractModel {
     
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), name, brand, specification, description, price, photoUrl);
+        return Objects.hash(super.hashCode(), name, brand, specification, description, price, quantity, photoUrl);
     }
 }
