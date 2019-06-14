480bc05d6051320fbe361256aaee7113a7bb6607
Toth-Gabor <upcgabor@gmail.com>
2019-06-14 19:35:28 +0200

    implemented findAllOrders and fetchOrder methods

---
+++ src/main/java/com/codecool/web/dao/database/DatabaseOrderDao.java
@@ -0,0 +1,41 @@
+package com.codecool.web.dao.database;
+
+import com.codecool.web.dao.OrderDao;
+import com.codecool.web.model.Order;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseOrderDao extends AbstractDao implements OrderDao {
+    
+    private String sql;
+    
+    DatabaseOrderDao(Connection connection) {
+        super(connection);
+    }
+    
+    @Override
+    public List<Order> findAllOrders() throws SQLException {
+        List<Order> ordersList = new ArrayList<>();
+        sql = "SELECT orders.order_id, user_id, order_date, product_id, quantity FROM orders\n" +
+                "    INNER JOIN line_item ON orders.order_id = line_item.order_id;";
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                ordersList.add(fetchOrder(resultSet));
+            }
+            return ordersList;
+        }
+    }
+
+    
+    private Order fetchOrder(ResultSet resultSet) throws SQLException {
+        int orderId = resultSet.getInt("order_id");
+        int userId = resultSet.getInt("user_id");
+        int productId = resultSet.getInt("product_id");
+        int quantity = resultSet.getInt("quantity");
+        Timestamp timestamp = resultSet.getTimestamp("order_date");
+        return new Order(orderId, userId, productId, quantity, timestamp);
+    }
+}
