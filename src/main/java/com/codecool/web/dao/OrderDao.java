9cd04bb86f484a1f69785892f1d40875afca2933
Toth-Gabor <upcgabor@gmail.com>
2019-06-14 19:32:48 +0200

    created OrderDao interface

---
+++ src/main/java/com/codecool/web/dao/OrderDao.java
@@ -0,0 +1,18 @@
+package com.codecool.web.dao;
+
+import com.codecool.web.model.Order;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.List;
+
+public interface OrderDao {
+
+    List<Order> findAllOrders() throws SQLException;
+    List<Order> findOrdersByUserId(int userId) throws SQLException;
+    List<Order> findOrdersByProductId(int productId) throws SQLException;
+    List<Order> findOrdersByDateWhatOlderThan(Timestamp timestamp) throws SQLException;
+    List<Order> findOrdersByDateWhatYoungerThan(Timestamp timestamp) throws SQLException;
+    void deleteOrder(int orderId) throws SQLException;
+    
+}
