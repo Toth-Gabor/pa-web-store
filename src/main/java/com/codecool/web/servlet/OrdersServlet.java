e8606888ebd8101c43faf1efb694049615bac13d
Toth-Gabor <upcgabor@gmail.com>
2019-06-15 08:34:11 +0200

    added doOption method

--- src/main/java/com/codecool/web/servlet/OrdersServlet.java
+++ src/main/java/com/codecool/web/servlet/OrdersServlet.java
@@ -6,12 +6,13 @@ import com.codecool.web.service.OrderService;
 import com.codecool.web.service.simple.SimpleOrderService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-
+@WebServlet("protected/orders")
 public class OrdersServlet extends AbstractServlet {
     
     @Override
@@ -26,13 +27,26 @@ public class OrdersServlet extends AbstractServlet {
     }
     
     @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (Connection connection = getConnection(req.getServletContext())){
+            OrderDao orderDao = new DatabaseOrderDao(connection);
+            OrderService orderService = new SimpleOrderService(orderDao);
+            String param = req.getParameter("param");
+            String fetch = req.getParameter("fetch");
+            sendMessage(resp, HttpServletResponse.SC_OK, orderService.methodSelector(param, fetch));
+        } catch (SQLException e) {
+            handleSqlError(resp, e);
+        }
+    }
+    
+    @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try (Connection connection = getConnection(req.getServletContext())){
             OrderDao orderDao = new DatabaseOrderDao(connection);
             OrderService orderService = new SimpleOrderService(orderDao);
             String orderId = req.getParameter("orderId");
             orderService.cancelOrder(orderId);
-            sendMessage(resp, HttpServletResponse.SC_OK, orderService.getAllOrders());
+            sendMessage(resp, HttpServletResponse.SC_OK, "Order has been canceled!");
         } catch (SQLException e) {
             handleSqlError(resp, e);
         }
