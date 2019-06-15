28abffaf79ba8e07802a92183d7d05cfdd79a14f
Toth-Gabor <upcgabor@gmail.com>
2019-06-15 18:35:03 +0200

    added showAllOrders functions

--- src/main/webapp/admin.js
+++ src/main/webapp/admin.js
@@ -1,3 +1,21 @@
 function onAdminButtonClicked() {
-    snackBar("Under construction!");
+    showContents(['topnav', 'admin-content']);
+}
+
+function showAllOrders() {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', showAllOrdersResponse);
+    xhr.addEventListener('error', onNetworkError);
+    xhr.open('GET', 'protected/orders');
+    xhr.send();
+
+}
+
+function showAllOrdersResponse() {
+    if (this.status === OK) {
+        showContents(['topnav', 'admin-content', 'orders']);
+        onAllOrdersListLoad(JSON.parse(this.responseText));
+    } else {
+        onOtherResponse(ordersContentDivEl, this);
+    }
 }
