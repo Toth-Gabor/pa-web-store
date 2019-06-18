a0779dfd07cf62034429c793f39ab4b9eeb87cf6
Toth-Gabor <upcgabor@gmail.com>
2019-06-18 16:34:29 +0200

    Created Cart class

---
+++ src/main/java/com/codecool/web/model/Cart.java
@@ -0,0 +1,40 @@
+package com.codecool.web.model;
+
+import java.util.List;
+
+public class Cart {
+    
+    private int productId;
+    private String name;
+    private int price;
+    private int Quantity;
+    private int userId;
+    
+    public int getProductId() {
+        return productId;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getPrice() {
+        return price;
+    }
+    
+    public int getQuantity() {
+        return Quantity;
+    }
+    
+    public int getUserId() {
+        return userId;
+    }
+    
+    public Cart(int productId, String name, int price, int quantity, int userId) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        Quantity = quantity;
+        this.userId = userId;
+    }
+}
